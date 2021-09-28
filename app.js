54ec43f84414acbc16e34fc8e81c2348b8b40585
KTJaakson <karl-tauno.jaakson@khk.ee>
2021-09-28 10:19:45 +0300

    funktsioonid2

--- app.js
+++ app.js
@@ -1,70 +1,41 @@
-// for tsükkel
-
-console.log('for tsükkel');
-for (let i = 0; i < 10; i++) {
-	if(i == 5){
-		continue;
-	}
-	console.log(i)
-};
-
-// while tsükkel
-
-console.log('while tsükkel');
-
-let i = 0;
-while(i < 10){
-	console.log(i);
-	i++;
+// funktsiooni defineerimine (deklareemine)
+function greeting(firstname, lastname){
+	return 'Tere, ' + firstname + ' ' + lastname;
 }
+//funktsioonide kutsumine tööle
 
-// do while
-
-
-console.log('do while tsükkel');
-
-let j = 0;
-do {
-	console.log(j);
-	j++;
-} while(j < 10);
+let greet = greeting('Karl', 'Jaakson');
+console.log(greet);
 
 
-// tsüklid ja massiivid
 
-console.log('massiivid ja tsüklid')
-const cars = ['Ford', 'Honda', 'Toyota'];
-
-// for tsükliga
-for(let i = 0; i < cars.length; i++) {
-	console.log(cars[i]);
-}
 
 console.log();
+//funktsioon avaldamise kujul
+const square = function(number = 3){
+	return number * number;
+}
+console.log(square());
 
-//forEach - callback function
+//
+(function(){
 
-cars.forEach(function(element, index, array){
-	console.log(`cars[${index}] = ${element}`);
-	console.log(array);
-
-});
-
-// forEach - arrow function
-cars.forEach((element, index) => {
-	console.log(`cars[${index}] = ${element}`);
-});
-
-// tsüklid ja objektid
-
-const person = {
-	firstname: 'Kadi',
-	surname: 'Tamm',
-	age: 25
 }
 
-// for in tsükkel
-for(let key in person){
-	console.log(`${key} = ${person[key]}`)
+//objekti see defineeritud funktsioonid
+console.log();
+const todo = {
+	add: function(){
+		console.log('add todo ...');
+	}
+	edit: function(id){
+		console.log(`edit todo nr ${id} ...`);
+	}
+	delete: function(){
+		console.log('delete todo ...');
+	}
 }
 
+todo.add();
+todo.edit(5);
+todo.delete();
