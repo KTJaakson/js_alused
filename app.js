b1356e83e7cd9c0d0257076e1acacc76c5645a15
KTJaakson <karl-tauno.jaakson@khk.ee>
2021-10-08 12:19:02 +0300

    submit

--- app.js
+++ app.js
@@ -1,36 +1,9 @@
-let val;
+const li = document.createElement('li');
 
-const list = document.querySelector('ul');
-const listItem = document.querySelector('li:first-child');
+li className = 'collection-item';
 
-val = list;
-val = listItem;
+li.appendChild(document.createTextNode('Study element creation'));
 
-val = list.childNodes;
-val = list.childNodes[0];
-val = list.childNodes[0].nodeName;
-val = list.childNodes[0].nodeType;
+list.appendChild(li);
 
-val = list.children;
-val = list.children[1];
-list.children[1].textContent = 'Study XML';
-
-val = list.children[2].children;
-list.children[2].children[0].id = 'test-id';
-
-val = list.firstChild;
-val = list.firstElementChild;
-
-val = list.lastChild;
-val = list.lastElementChild;
-
-val = list.childElementCount;
-
-val = listItem.parentNode;
-val = listItem.parentElement;
-val = listItem.parentElement.parentElement;
-
-val = listItem.nextSibling;
-val = listItem.nextElementSibling.nextElementSibling.nextElementSibling;
-
-console.log(val);
+console.log(li);
